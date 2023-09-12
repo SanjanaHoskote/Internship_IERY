9d6e01b1fedce53ba1dfe720c43378e682af7e10
SanjanaHoskote <128903809+SanjanaHoskote@users.noreply.github.com>
2023-09-12 19:07:08 +0530

    Update Synchronous FIFO.v

--- Synchronous FIFO.v
+++ Synchronous FIFO.v
@@ -1,32 +1,33 @@
-CODE:
-module synchronous_fifo #(parameter DEPTH=8, DATA_WIDTH=8) (
-  input clk, rst_n,
-  input w_en, r_en,
-  input [DATA_WIDTH-1:0] data_in,
-  output reg [DATA_WIDTH-1:0] data_out,
+// Code your design here
+
+module synchronous_fifo #(parameter fifo_depth=8, DATAWIDTH=8) (
+  input clk, reset_n,
+  input wr_en, r_en,
+  input [DATAWIDTH-1:0] data_in,
+  output reg [DATAWIDTH-1:0] data_out,
   output full, empty
 );
   
-  reg [$clog2(DEPTH)-1:0] w_ptr, r_ptr;
-  reg [DATA_WIDTH-1:0] fifo[DEPTH];
+  reg [2:0] w_ptr, r_ptr;
+  reg [DATAWIDTH-1:0] fifo[fifo_depth];
   
-  // Set Default values on reset.
+  // Sreset
   always@(posedge clk) begin
-    if(!rst_n) begin
+    if(!reset_n) begin
       w_ptr <= 0; r_ptr <= 0;
       data_out <= 0;
     end
   end
   
-  // To write data to FIFO
+  // write operation
   always@(posedge clk) begin
-    if(w_en & !full)begin
+    if(wr_en & !full)begin
       fifo[w_ptr] <= data_in;
       w_ptr <= w_ptr + 1;
     end
   end
   
-  // To read data from FIFO
+  // read operation 
   always@(posedge clk) begin
     if(r_en & !empty) begin
       data_out <= fifo[r_ptr];
@@ -37,4 +38,5 @@ module synchronous_fifo #(parameter DEPTH=8, DATA_WIDTH=8) (
   assign full = ((w_ptr+1'b1) == r_ptr);
   assign empty = (w_ptr == r_ptr);
 endmodule
+
 
